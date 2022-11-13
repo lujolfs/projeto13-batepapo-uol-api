689f48b989238bc368368442699a457e29c86c18
lujolfs <lucas.j.frank@gmail.com>
2022-11-13 14:43:50 -0300

    Renderização de mensagens

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import express, { response } from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
+import dayjs from "dayjs";
+import joi from "joi";
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+const calendario = dayjs().format('HH:MM:ss')
 
 const mongoClient = new MongoClient("mongodb://localhost:27017");
 let db;
-let name;
+
 
 
 mongoClient
@@ -40,11 +43,42 @@ app.post("/participants", (req, res) => {
     })
     .then(() => {
         res.status(201).send("Usuário criado com sucesso.")
-    })
+        db.collection("messages").insertOne({
+            'from': body, 
+            'to': 'Todos',
+            'text': 'entra na sala...',
+            'type': 'status',
+            'time': calendario})
+        })
     .catch(err => {
         res.status(500).send(err);
     });
 });
 
+app.get("/messages", (req, res) => {
+    db.collection("messages")
+    .find()
+    .toArray()
+    .then((msg) => {
+        res.send(msg);
+    }).catch(err => {
+        console.log(err)
+        res.sendStatus(500);
+    });
+});
+
+app.post("/messages", (req, res) => {
+    const body = req.body.name
+    db.collection("participants")
+    .insertOne({
+        "name": body, "lastStatus": Date.now()
+    })
+    .then(() => {
+        res.status(201).send("Usuário criado com sucesso.")
+    })
+    .catch(err => {
+        res.status(500).send(err);
+    });
+});
 
 app.listen(5000);
